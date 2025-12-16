91eab868232317e2165d00b458d875f201b1b723
Linztark <anasilvesttttre@gmail.com>
2025-12-16 10:28:02 +0100

    publicado / despublicado

--- src/main/java/net/ausiasmarch/persutil/repository/SilvestreRepository.java
+++ src/main/java/net/ausiasmarch/persutil/repository/SilvestreRepository.java
@@ -1,9 +1,17 @@
 package net.ausiasmarch.persutil.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.ausiasmarch.persutil.entity.SilvestreEntity;
 
 public interface SilvestreRepository extends JpaRepository<SilvestreEntity, Long> {
+
+	Page<SilvestreEntity> findByPublicadoTrue(Pageable oPageable);
+
+	Page<SilvestreEntity> findByPublicadoFalse(Pageable oPageable);
+
+	SilvestreEntity findByIdAndPublicadoTrue(Long id);
 
 }
